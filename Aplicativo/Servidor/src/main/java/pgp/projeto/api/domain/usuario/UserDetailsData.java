4154cfbace2fb44e092607b724427f96bd2dbc2a
Gladistony Silva Lins <94619256+Gladistony@users.noreply.github.com>
2023-12-01 06:46:42 -0300

    Revert "testando"

--- Aplicativo/Servidor/src/main/java/pgp/projeto/api/domain/usuario/UserDetailsData.java
+++
@@ -1,9 +0,0 @@
-package pgp.projeto.api.domain.usuario;
-
-import java.time.LocalDate;
-
-public record UserDetailsData(Long id, String nome, String email, LocalDate dataNascimento) {
-    public UserDetailsData(UserAccount user) {
-        this(user.getId(), user.getNome(), user.getLogin(), user.getDataNascimento());
-    }
-}
