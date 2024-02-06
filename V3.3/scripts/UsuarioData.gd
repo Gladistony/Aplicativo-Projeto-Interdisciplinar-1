2dfb779150a3f29e0794c063fd1f83d40b74f731
Gladistony Silva Lins <bistone3000@hotmail.com>
2024-02-06 14:50:43 -0300

    Adicionado Recursos a 3.3

---
+++ V3.3/scripts/UsuarioData.gd
@@ -0,0 +1,39 @@
+extends Node
+
+var _data = {}
+
+
+func _fileExist(name):
+	var dir = Directory.new()
+	return  dir.file_exists(name)
+
+
+func _logOff():
+	_data.login = ""
+	_data.senha = ""
+	_data.token = true
+	_data.loginStatus = false
+
+func _ready():
+	_data.login = ""
+	_data.senha = ""
+	_data.token = true
+	_data.loginStatus = false
+	_loadData()
+
+func _loadData():
+	if _fileExist("res://dadosLocais.datLife"):
+		var file = File.new()
+		file.open("res://dadosLocais.datLife", File.READ)
+		#var arq = FileAccess.open("",FileAccess.READ)
+		var jsonstr =  file.get_as_text()
+		file.close()
+		var data = JSON.new()
+		data.parse(jsonstr)
+		_data = data.data
+
+func _saveData():
+	var file = File.new()
+	file.open("res://dadosLocais.datLife", File.WRITE)
+	file.store_line(JSON.stringify(_data))
+	file.close()
