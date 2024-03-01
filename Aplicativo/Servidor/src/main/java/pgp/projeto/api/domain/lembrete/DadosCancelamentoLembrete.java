d2b81c3771f976179194f1dcf67acc1887b88060
Erick Jonathan <zxjonathanxz22@gmail.com>
2024-03-01 15:19:29 -0300

    removendo servidor para outras ocasiões

--- Aplicativo/Servidor/src/main/java/pgp/projeto/api/domain/lembrete/DadosCancelamentoLembrete.java
+++
@@ -1,9 +0,0 @@
-package pgp.projeto.api.domain.lembrete;
-
-import jakarta.validation.constraints.NotNull;
-
-public record DadosCancelamentoLembrete(
-        @NotNull
-        Long idLembrete
-       ) {
-}
